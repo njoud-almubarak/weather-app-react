9c90f8fac4843fe89293daf6529bd37bfccee827
njoud-almubarak <102929499+njoud-almubarak@users.noreply.github.com>
2022-06-23 12:52:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,17 +31,13 @@ function App() {
   return (
     <>
    
-    <img
-    src="https://media.istockphoto.com/photos/weather-forecast-concept-picture-id531889697?k=20&m=531889697&s=612x612&w=0&h=Pl_z87kLerrlN76k83OtXhURIm5P0YR83tBHnTsMKPU="
-    width='1400rem'
-    height='400rem'
-    alt='wether '
-    className='rounded mx-auto d-block' ></img>
+   
     
     <div className="App">
 
+    <h4 className='text-center mt-3 mb-3 ml-60  w-100'>Weather App </h4>
     <input onChange={(e) => setCountryName(e.target.value)} value={counterName} className="form-control" type="text" placeholder="Enter country name " />
-    <h4 className='text-center mt-3 ml-60  w-100'>The Weather in {' '} <span>{counterName}</span> is: {""}</h4>
+    <h4 className='text-center mt-3 ml-60  w-100'>The Weather in {' '} <span>{counterName}</span>  {""}</h4>
     <h4 className='text-center mt-3 ml-60  w-100'> Temperature : {temperature} {""}</h4>
     <h4 className='text-center mt-3 ml-60  w-100'> wind : {wind} {""} </h4>
  
